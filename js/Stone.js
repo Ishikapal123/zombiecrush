be1af23b94a496a48e7abc5db565eef873c02cd4
Ishikapal123 <85355470+Ishikapal123@users.noreply.github.com>
2021-08-30 20:21:19 +0530

    Add files via upload

---
+++ js/Stone.js
@@ -0,0 +1,27 @@
+class Stone
+{
+  constructor(x, y, w,h) 
+  {
+    let options = {
+     isStatic:false
+    };
+    
+    this.body = Bodies.rectangle(x, y, w, h, options);
+    this.w = w;
+    this.h = h;
+    World.add(world, this.body);
+  }
+
+  display() {
+    let pos = this.body.position;
+    push();
+    ellipseMode(CENTER);
+    noStroke();
+    fill("white");
+    ellipse(pos.x,pos.y, this.w, this.h);
+    pop();
+  }
+}
+
+    
+
